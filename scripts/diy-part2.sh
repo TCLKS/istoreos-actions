199028e968782ef0354c89bc35e6879260a350ce
TCLKS <66046233+TCLKS@users.noreply.github.com>
2025-04-18 23:39:47 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -250,7 +250,6 @@ cp -rf ../../kiddin9/luci-app-unblockneteasemusic/* luci-app-unblockneteasemusic
 mkdir quectel_QMI_WWAN
 mkdir quectel_cm_5G
 mkdir quectel_MHI
-mkdir fibocom_MHI
 mkdir luci-app-hypermodem
 mkdir fibocom-dial
 mkdir fibocom_QMI_WWAN
@@ -260,7 +259,6 @@ mkdir meig_QMI_WWAN
 cp -rf ../../Modem-Support/quectel_QMI_WWAN/* quectel_QMI_WWAN
 cp -rf ../../Modem-Support/quectel_cm_5G/* quectel_cm_5G
 cp -rf ../../Modem-Support/quectel_MHI/* quectel_MHI
-cp -rf ../../Modem-Support/fibocom_MHI/* fibocom_MHI
 cp -rf ../../Modem-Support/luci-app-hypermodem/* luci-app-hypermodem
 cp -rf ../../Modem-Support/fibocom-dial/* fibocom-dial
 cp -rf ../../Modem-Support/fibocom_QMI_WWAN/* fibocom_QMI_WWAN
